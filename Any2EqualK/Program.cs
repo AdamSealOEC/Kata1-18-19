ac147ae91876e26965c2a8a38647528655400825
Adam Seal <adam.seal@oeconnection.com>
2019-01-22 08:58:42 -0500

    Uploading files

---
+++ Any2EqualK/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Any2EqualK
+{
+    class Program
+    {
+        static bool Main(List<int> nums, int k)
+        {
+            for (int i = 0; i < nums.Count - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Count; j++)
+                {
+                    if ((nums[i] + nums[j]) == k)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
